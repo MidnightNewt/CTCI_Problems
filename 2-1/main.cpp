a600f8b0330d13d660d6511a2fd45c3aa66ac333
Anna Hyldahl <ahyldahl@u.washington.edu>
2014-07-28 17:56:21 -0700

    Adding some missed files from last commit.  Changed file names to main.cpp.

---
+++ 2-1/main.cpp
@@ -0,0 +1,26 @@
+//2.1: Write code to remove duplicates from an unsorted linked list.  Also, how would you solve this problem if a temporary buffer is not allowed?
+
+//Ideas: 
+//In place merge sort:
+//Changes nearby values, good for linked list
+//Recursive merge of smaller and smaller subarrays
+
+#include <iostream>
+using namespace main;
+#include <linkedlist>
+
+//Forward declarations
+void removeDuplicates(linkedlist);
+
+int main()
+{
+
+
+    return 0;
+}
+
+void removeDuplicates(linkedlist lnk)
+{
+
+
+}
